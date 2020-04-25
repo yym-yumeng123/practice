f0d7ed1a5dbcb7849c569b2a468d0da89695907b
yym-yumeng123 <18026493873@163.com>
2020-04-25 21:12:12 +0800

    改成 ES3

--- BIND/src/index.js
+++ BIND/src/index.js
@@ -1,8 +1,21 @@
-function bind(asThis, ...args) {
+var slice = Array.prototype.slice
+
+function bind() {
+  // 第一个参数 this
+  var asThis = arguments[0]
+  // 第一个参数不要
+  var args = slice.call(arguments, 1)
   // this 就是函数
-  const fn = this
-  return function (...argu) {
-    return fn.call(asThis, ...args, ...argu)
+  var fn = this
+
+  if(typeof fn !== 'function') {
+    throw new Error('bind 必须调用在函数身上')
+  }
+
+  return function () {
+    var args2 = slice.call(arguments, 0)
+    // return fn.call(asThis, ...args, ...argu)
+    return fn.apply(asThis, args.concat(args2))
   }
 }
 
@@ -10,4 +23,17 @@ module.exports = bind
 
 if (!Function.prototype.bind) {
   Function.prototype.bind = bind
+}
+
+/**
+ * ES6版本的
+ * @param {arguments[0]} asThis 
+ * @param  {slice(1)} args 
+ */
+function _bind(asThis, ...args) {
+  // this 就是函数
+  const fn = this
+  return function (...argu) {
+    return fn.call(asThis, ...args, ...argu)
+  }
 }
