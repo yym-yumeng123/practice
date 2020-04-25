6fd9d1d3f44caeebabfed3803dea52700a730b48
yym-yumeng123 <18026493873@163.com>
2020-04-25 17:51:35 +0800

    改成 ts

--- EVENTHUB/src/index.ts
+++ EVENTHUB/src/index.ts
@@ -4,23 +4,23 @@ class EventHub {
    * 'A报纸': [fn1, fn2...]
    * 'B报纸: [fn1, fn2...]
    */
-  cache = {}
+  private cache: { [key: string]: Array<(data: unknown) => void> } = {}
 
   // 注册 or 监听事件
-  on(eventName, fn) {
+  on(eventName: string, fn: (data: unknown) => void) {
     // 把 fn 推进 this.cache[eventname] 数组
     this.cache[eventName] = this.cache[eventName] || []
     this.cache[eventName].push(fn)
   }
 
   // 触发事件
-  emit(eventName, params?) {
+  emit(eventName: string, params?: unknown) {
     // 把 this.cache[eventName] 数组里面的 fn 依次调用
     ;(this.cache[eventName] || []).forEach((fn) => fn(params))
   }
 
   // 把 fn 从 this.cache[eventname] 数组 删除
-  off(eventName, fn) {
+  off(eventName: string, fn: (data: unknown) => void) {
     let index = indexOf(this.cache[eventName], fn)
     if (index === -1) return
     this.cache[eventName].splice(index, 1)
@@ -31,8 +31,8 @@ export default EventHub
 
 /**
  * helper indexOf
- * @param array 
- * @param item 
+ * @param array
+ * @param item
  */
 function indexOf(array, item) {
   if (array === undefined) {
--- EVENTHUB/test/index.ts
+++ EVENTHUB/test/index.ts
@@ -1,13 +1,15 @@
 import EventHub from "../src/index"
 
-const test1 = (message) => {
-	const eventHub = new EventHub()
-	console.assert(eventHub instanceof Object === true, "event 是个对象")
-	console.log(message)
+type TestCase = (message: string) => void
+
+const test1: TestCase = (message) => {
+  const eventHub = new EventHub()
+  console.assert(eventHub instanceof Object === true, "event 是个对象")
+  console.log(message)
 }
 
-const test2 = (message) => {
-	const eventHub = new EventHub()
+const test2: TestCase = (message) => {
+  const eventHub = new EventHub()
   let called = false
   eventHub.on("xxx", (data) => {
     called = true
@@ -17,12 +19,12 @@ const test2 = (message) => {
   eventHub.emit("xxx", "测试 触发了 参数")
 
   setTimeout(() => {
-		console.assert(called === true)
-		console.log(message)
+    console.assert(called === true)
+    console.log(message)
   }, 1000)
 }
 
-const test3 = (message) => {
+const test3: TestCase = (message) => {
   const eventHub = new EventHub()
   let called = false
   const fn1 = () => {
@@ -34,11 +36,11 @@ const test3 = (message) => {
   eventHub.emit("yyy")
 
   setTimeout(() => {
-		console.assert(called === false)
-		console.log(message)
-	}, 1000)
+    console.assert(called === false)
+    console.log(message)
+  }, 1000)
 }
 
-test1('EventHub 可以创建对象')
-test2('.on 之后, .emit会触发 .on的函数')
-test3('.off 可以用')
+test1("EventHub 可以创建对象")
+test2(".on 之后, .emit会触发 .on的函数")
+test3(".off 可以用")
