b1db5eff9420ab018b5c5b42ec5ec876b654ac9f
yym-yumeng123 <18026493873@163.com>
2020-04-25 17:27:11 +0800

    完善 eventHub

--- EVENTHUB/src/index.ts
+++ EVENTHUB/src/index.ts
@@ -1,23 +1,50 @@
 // 发布订阅模式
 class EventHub {
-	/**
-	 * 'A报纸': [fn1, fn2...]
-	 * 'B报纸: [fn1, fn2...]
-	 */
-	cache = {}
+  /**
+   * 'A报纸': [fn1, fn2...]
+   * 'B报纸: [fn1, fn2...]
+   */
+  cache = {}
 
   // 注册 or 监听事件
   on(eventName, fn) {
-		// 把 fn 推进 this.cache[eventname] 数组
-		this.cache[eventName] = this.cache[eventName] || []
-		this.cache[eventName].push(fn)
-	}
+    // 把 fn 推进 this.cache[eventname] 数组
+    this.cache[eventName] = this.cache[eventName] || []
+    this.cache[eventName].push(fn)
+  }
 
   // 触发事件
-  emit(eventName, params) {
-		// 把 this.cache[eventName] 数组里面的 fn 依次调用
-		(this.cache[eventName] || []).forEach(fn => fn(params))
-	}
+  emit(eventName, params?) {
+    // 把 this.cache[eventName] 数组里面的 fn 依次调用
+    ;(this.cache[eventName] || []).forEach((fn) => fn(params))
+  }
+
+  // 把 fn 从 this.cache[eventname] 数组 删除
+  off(eventName, fn) {
+    let index = indexOf(this.cache[eventName], fn)
+    if (index === -1) return
+    this.cache[eventName].splice(index, 1)
+  }
 }
 
-export default EventHub;
+export default EventHub
+
+/**
+ * helper indexOf
+ * @param array 
+ * @param item 
+ */
+function indexOf(array, item) {
+  if (array === undefined) {
+    return -1
+  }
+  let index = -1
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      index = i
+      break
+    }
+  }
+
+  return index
+}
--- EVENTHUB/test/index.ts
+++ EVENTHUB/test/index.ts
@@ -1,14 +1,44 @@
-import EventHub from "../src/index";
+import EventHub from "../src/index"
 
-const eventHub = new EventHub();
+const test1 = (message) => {
+	const eventHub = new EventHub()
+	console.assert(eventHub instanceof Object === true, "event 是个对象")
+	console.log(message)
+}
 
-console.assert(eventHub instanceof Object === true, "event 是个对象");
+const test2 = (message) => {
+	const eventHub = new EventHub()
+  let called = false
+  eventHub.on("xxx", (data) => {
+    called = true
+    console.assert(data === "测试 触发了 参数")
+  })
 
-let called = false
-eventHub.on("xxx", data => {
-	called = true
-	console.log('called', called)
-	console.assert(data === '杨雨蒙在家1')
-});
+  eventHub.emit("xxx", "测试 触发了 参数")
 
-eventHub.emit("xxx", '杨雨蒙在家');
+  setTimeout(() => {
+		console.assert(called === true)
+		console.log(message)
+  }, 1000)
+}
+
+const test3 = (message) => {
+  const eventHub = new EventHub()
+  let called = false
+  const fn1 = () => {
+    called = true
+  }
+
+  eventHub.on("yyy", fn1)
+  eventHub.off("yyy", fn1)
+  eventHub.emit("yyy")
+
+  setTimeout(() => {
+		console.assert(called === false)
+		console.log(message)
+	}, 1000)
+}
+
+test1('EventHub 可以创建对象')
+test2('.on 之后, .emit会触发 .on的函数')
+test3('.off 可以用')
