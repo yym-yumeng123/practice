52c4db4114f1537e9d1fed0f9882c5fd89b4a73e
yym-yumeng123 <18026493873@163.com>
2020-05-05 21:31:44 +0800

    feat: 递归深克隆

--- DeepClone/src/index.js
+++ DeepClone/src/index.js
@@ -1,5 +1,29 @@
-function deepClone() {
-
+function deepClone(source) {
+  // source 是对象吗
+  if (source instanceof Object) {
+    if (source instanceof Array) {
+      const dist = new Array()
+      for (let key in source) {
+        dist[key] = deepClone(source[key])
+      }
+      return dist
+    } else if (source instanceof Function) {
+      const dist = function () {
+        return source.apply(this, arguments)
+      }
+      for (let key in source) {
+        dist[key] = deepClone(source[key])
+      }
+      return dist
+    } else {
+      const dist = new Object()
+      for (let key in source) {
+        dist[key] = deepClone(source[key])
+      }
+      return dist
+    }
+  }
+  return source
 }
 
 module.exports = deepClone
--- DeepClone/test/index.js
+++ DeepClone/test/index.js
@@ -10,4 +10,78 @@ describe("deepClone", () => {
   it("是个函数", () => {
     assert.isFunction(deepClone)
   })
+  it("能够赋值基本类型", () => {
+    const num = 42
+    const num2 = deepClone(num)
+    assert(num === num2)
+
+    const str = "42"
+    const str2 = deepClone(str)
+    assert(str === str2)
+
+    const bool = true
+    const bool2 = deepClone(bool)
+    assert(bool === bool2)
+
+    const unde = undefined
+    const unde2 = deepClone(unde)
+    assert(unde === unde2)
+
+    const empty = null
+    const empty2 = deepClone(empty)
+    assert(empty === empty2)
+
+    const sym = Symbol()
+    const sym2 = deepClone(sym)
+    assert(sym === sym2)
+  })
+
+  describe("对象", () => {
+    it("能够复制普通对象", () => {
+      const obj = {
+        name: "yym",
+        child: {
+          name: "zs",
+        },
+      }
+      const obj2 = deepClone(obj)
+
+      assert(obj !== obj2)
+      assert(obj.name === obj2.name)
+      assert(obj.child !== obj2.child)
+      assert(obj.child.name === obj2.child.name)
+    })
+
+    it("能够复制数组对象", () => {
+      const arr = [
+        [1, 2],
+        [3, 4],
+        [5, 6],
+      ]
+      const arr2 = deepClone(arr)
+
+      assert(arr !== arr2)
+      assert(arr[0] !== arr2[0])
+      assert(arr[1] !== arr2[1])
+      assert(arr[2] !== arr2[2])
+      assert.deepEqual(arr, arr2)
+    })
+
+    it("能够复制函数", () => {
+      const fn = function () {
+        return 1
+      }
+      fn.obj = {
+        name: {
+          age: 12,
+        },
+      }
+      const fn2 = deepClone(fn)
+      assert(fn !== fn2)
+      assert(fn.obj.name.age === fn2.obj.name.age)
+      assert(fn.obj.name !== fn2.obj.name)
+      assert(fn.obj !== fn2.obj)
+      assert(fn() === fn2())
+    })
+  })
 })
