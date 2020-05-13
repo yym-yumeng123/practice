d9da102fe1bbce7cef72d52e2ef5fd3b1c893dc0
yym-yumeng123 <18026493873@163.com>
2020-05-13 23:14:36 +0800

    add 广播

--- Backgammon/server.js
+++ Backgammon/server.js
@@ -1,11 +1,13 @@
-const WebSocket = require('ws');
+const WebSocket = require("ws")
 
-const wss = new WebSocket.Server({ port: 8081 });
+const wss = new WebSocket.Server({ port: 8081 })
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
-  ws.send('something');
-});
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data)
+      }
+    })
+  })
+})
