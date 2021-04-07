caa5fd8bd9f6a99812111108df48161864dacdc4
yym-yumeng123 <18026493873@163.com>
2021-04-07 15:50:41 +0800

    类的修饰符: private protected public static

--- practice/ts-basic-demo/class.ts
+++ practice/ts-basic-demo/class.ts
@@ -1,8 +1,22 @@
+/**
+ * 修饰符
+ * 1. public 公有的
+ * 2. private 私有的
+ * 3. protected 保护, 只能继承
+ * 4. readonly 只读
+ * 5. static 静态
+ */
 class Animal {
-  name: string
+  readonly name: string
+  static categoies: string[] = ['animal', 'bird']
+  static isAnimal(a) {
+    return a instanceof Animal
+  }
   constructor(name: string) {
     this.name = name
   }
+
+
 
   run() {
     return `${this.name} is running`
@@ -11,6 +25,9 @@ class Animal {
 
 const snake = new Animal("yym")
 console.log(snake.run())
+console.log(snake.name)
+// snake.name = "改名字"
+console.log(snake.name)
 
 class Dog extends Animal {
   bark() {
@@ -33,3 +50,20 @@ class Cat extends Animal {
 }
 const mao = new Cat("mao")
 console.log(mao.run())
+
+// 私有 private
+class Animal1 {
+  private name: string
+  constructor(name: string) {
+    this.name = name
+  }
+
+  run() {
+    return `${this.name} is running`
+  }
+}
+
+const snake1 = new Animal1("yym")
+console.log(snake1.run())
+// 属性“name”为私有属性，只能在类“Animal1”中访问
+// console.log(snake1.name);
