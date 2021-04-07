d57d0a47369cf989fad4c0db3ab46705189f2592
yym-yumeng123 <18026493873@163.com>
2021-04-07 11:43:03 +0800

    ts 数据类型: any or | 联合类型

--- practice/ts-basic-demo/basic-types.ts
+++ practice/ts-basic-demo/basic-types.ts
@@ -12,3 +12,16 @@ let n: null = null
 
 // undefined null 是所有类型的子类型
 let num: number = undefined
+
+// 无法确定类型 any
+let notSure: any = 4
+notSure = true
+notSure = 'yym'
+notSure.myName
+
+// 允许部分类型: 联合类型 |
+let numberOrString: number | string = '111'
+numberOrString = 123
+
+
+
