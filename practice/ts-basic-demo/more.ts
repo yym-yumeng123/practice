bdd4a78018d94f2492ddf687f2e436486fecfca2
yym-yumeng123 <18026493873@163.com>
2021-04-08 17:37:34 +0800

    type as 类型别名 & 类型断言

---
+++ practice/ts-basic-demo/more.ts
@@ -0,0 +1,42 @@
+/**
+ * 类型别名: type aliases
+ */
+
+type PlusType = (x: number, y: number) => number
+function sum(x: number, y: number): number {
+  return x + y
+}
+
+const sum2: PlusType = sum
+
+// ----------------------------------
+
+type NameResolver = () => string
+type NameOrResolver = string | NameResolver
+function getName(n: NameOrResolver): string {
+  if (typeof n === "string") {
+    return n
+  } else {
+    return n()
+  }
+}
+
+/**
+ * 类型断言 type assertion
+ * as
+ */
+function getLength(input: string | number): number {
+  // 第一种断言 as
+  // const str = input as string
+  // if (str.length) {
+  //   return str.length
+  // } else {
+  //   const number = input as Number
+  //   return number.toString().length
+  // }
+
+  // 第二种断言
+  if ((<string>input).length) {
+    return (<string>input).length
+  }
+}
